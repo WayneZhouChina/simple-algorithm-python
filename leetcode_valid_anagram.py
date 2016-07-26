2418d1e4a292b8897669f8b96db200eb6095ed82
WayneZhouChina <cumtxhzyy@gmail.com>
2016-07-26 14:22:21 +0800

    leetcode valid anagram 242

---
+++ leetcode_valid_anagram.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
