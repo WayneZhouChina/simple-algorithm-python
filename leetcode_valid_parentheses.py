3742ee6f44c0062eb063e77903260d0961a3f85c
WayneZhouChina <cumtxhzyy@gmail.com>
2016-08-15 22:29:41 +0800

    leetcode valid parentheses [esay 20]

---
+++ leetcode_valid_parentheses.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for ch in s:
+            if ch == '(' or ch == '[' or ch == '{':
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+                if ch == ']' and stack[-1] != '[' or ch == ')' and stack[-1] != '(' or  ch == '}' and stack[-1] != '{':
+                    return False
+                stack.pop()
+
+        return True if not stack else False
