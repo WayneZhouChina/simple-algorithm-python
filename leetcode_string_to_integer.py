5a03b211361024d468fb9c722bb35b5c84037fd5
wayne_zhou <cumtxh_zyy@163.com>
2016-09-06 23:04:28 +0800

    leetcode string to int [esay ]

---
+++ leetcode_string_to_integer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+class Solution(object):
+	def myAtoi(self, str):
+		str = str.strip()
+		if str == "":
+			return 0
+		i = 0
+		sign = 1
+		ret = 0
+		MaxInt = (1 << 31) - 1
+
+		if str[i] == '+':
+			i -= 1
+		elif str[i] == '-':
+			i -= 1
+			sign = -1
+
+		for i in (i, len(str)):
+			if str[i] < '0' or str[i] > '9':
+				break
+
+			ret = ret * 10 + int(str[i])
+			if ret > sys.maxint:
+				break
+
+		ret *= sign
+		if ret >= MaxInt:
+			return MaxInt
+		if ret < MaxInt * -1:
+			return MaxInt *-1 -1
+	
+		return ret
+
+			
